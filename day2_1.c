827cd951bb71c079a39ffddc5a0ef3b49ebf9fd8
Amy Devine <adevine@gmail.com>
2023-12-08 12:01:02 -0600

    solved day 2

---
+++ day2_1.c
@@ -0,0 +1,206 @@
+// https://code.visualstudio.com/docs/cpp/config-mingw
+// https://www.tutorialspoint.com/c_standard_library/string_h.htm
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+const int WINRED = 12;       
+const int WINGREEN = 13;
+const int WINBLUE = 14;
+
+
+struct tuple {
+    int red;
+    int green;
+    int blue;
+};
+
+int winningGames[100];
+int allGames[100];
+
+int numWinningGames()
+{
+    int total = 0;
+
+    for(int i = 0; i < 100; i++)
+    {
+        if (winningGames[i] = 1)
+        {
+            total +=1;
+        }
+    }
+    return total;
+}
+
+int findRed(char* str)
+{
+    char* startPtr = str;
+    char* endPtr;
+    int retVal = 0;
+
+    endPtr = strstr(str, "red");
+    if(endPtr == NULL)
+        retVal = 0;
+    else
+    {
+        endPtr--;
+        *endPtr = '\n';
+        startPtr = --endPtr;
+        while( (startPtr[0]  != ' ') && (startPtr >= str) )
+        {
+            startPtr--;
+        }
+        startPtr++;        
+        retVal = atoi(startPtr);
+        *endPtr = ' ';
+    }
+    return retVal;
+}
+
+int findGreen(char* str)
+{
+    char* startPtr = str;
+    char* endPtr;
+    int retVal = 0;
+
+    endPtr = strstr(str, "green");
+    if(endPtr == NULL)
+        retVal = 0;
+    else
+    {
+        endPtr--;
+        *endPtr = '\n';
+        startPtr = --endPtr;
+        while( (startPtr[0]  != ' ') && (startPtr >= str) )
+        {
+            startPtr--;
+        }
+        startPtr++;
+        retVal = atoi(startPtr);
+        *endPtr = ' ';
+    }
+    return retVal;
+}
+
+int findBlue(char* str)
+{
+    char* startPtr = str;
+    char* endPtr;
+    int retVal = 0;
+
+    endPtr = strstr(str, "blue");
+    if(endPtr == NULL)
+        retVal = 0;
+    else
+    {
+        endPtr--;
+        *endPtr = '\n';
+        startPtr = --endPtr;
+        while( (startPtr[0] != ' ') && (startPtr >= str) )
+        {
+            startPtr--;
+        }
+        startPtr++;
+        retVal = atoi(startPtr);
+        *endPtr = ' ';
+    }
+    return retVal;
+}
+
+
+int parseLine(char* inputString, int lineNumber)
+{
+    struct tuple tup;
+    int gamePossible = 0;
+
+    // extract game number
+    char* startPtr;
+    char* endPtr;
+
+    inputString = strchr(inputString, ':');
+    if(inputString == NULL)
+    {
+        printf("error with input format");
+        return 0;
+    }
+
+    inputString +=2; //skip space
+
+    // find tuple
+    // gotta copy the string -- why?
+    //char copy[100] = '\n';
+    //strcpy(copy, inputString);
+
+    startPtr = strtok(inputString, ";");
+
+    while(startPtr!= NULL)
+    { 
+        tup.red = findRed(startPtr);
+        tup.green = findGreen(startPtr);
+        tup.blue = findBlue(startPtr);
+
+        // parse tuple
+        if ( (tup.red <= WINRED) && (tup.green <= WINGREEN) && (tup.blue <= WINBLUE))
+        {
+            startPtr = strtok(NULL, ";");
+            gamePossible = 1;
+            continue;
+        }
+        else
+        {
+            gamePossible = 0;
+            break; // this game isn't possible
+        }
+    }
+    return gamePossible;
+}
+
+int main(int argc, void* argv) 
+{
+    char testLine[] = "Game 1: 8 red, 4 green, 7 blue; 2 blue, 4 red;";
+
+    int totalValue = 0;
+    int gameNumber = 1;
+    int gamePossible = 0;
+
+    // Open and read the file
+    const char inputFileName[] = "day2_input.txt";
+
+    FILE* input = fopen(inputFileName, "r");
+  
+    if(input == NULL)
+    {
+        printf("Error opening file %s\n", input);
+        return -1;
+    }
+    
+    while(1)
+    {
+        printf("Game Number: %d\t", gameNumber);
+    
+        char inputString[2048];
+        inputString[0] = '\n';
+
+        char* result = fgets(inputString, sizeof(inputString), input);
+        if(result == NULL)
+        {
+            printf("Done!\n");
+            break;
+        }
+
+        gamePossible = parseLine(inputString, gameNumber);
+        printf(" is %s possible\n", gamePossible == 1? "": "not");
+
+        if(gamePossible)
+        {
+            totalValue += gameNumber;
+        }
+        // game 3, 5, 6, 10 should be skipped
+        gameNumber++;
+    }
+    printf("totalValue: %d\n", totalValue);
+
+    return totalValue;
+}
