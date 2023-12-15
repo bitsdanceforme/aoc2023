09a87598a2285405cee38489aa9bf85e7063946b
Amy Devine <adevine@gmail.com>
2023-12-15 10:59:46 -0600

    Works for 3 rows

--- day3_1.c
+++ day3_1.c
@@ -2,13 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void findNumbers(char* row, int offset)
+int findNumbers(char* row, int offset)
 {
     char* onePosition;
     char* twoPosition;
     char* threePosition;
 
     char* endOne, endTwo, endThree;
+    char* numberOne, *numberTwo, *numberThree;
+
+    int sum;
 
     onePosition = row + (offset * sizeof(char)) - 1;
     twoPosition = row + (offset * sizeof(char));
@@ -21,44 +24,60 @@ void findNumbers(char* row, int offset)
         // do I need to back up?
         while(1)
         {
-            onePosition = onePosition - sizeof(char);
-            if( (onePosition)[0] == '.')
+            onePosition = onePosition - sizeof(char);            
+            if( ((onePosition)[0] == '.') || (onePosition < row))
              break;
         }
         ++onePosition;
         // find end of string
-        char* temp = (char*)malloc(sizeof(char)*(sizeof(onePosition)+1));
-        temp[0] = '\n';
+        numberOne = strchr(onePosition, '.');
+        int length = 0;
 
-        memcpy(temp, onePosition, sizeof(onePosition));
-
-        char* number = strtok(temp, ".");
-
-        number = strtok(NULL, ".");
-        if(number > threePosition)
-            return;
-
-        number = strtok(NULL, ".");    
-        if(number > threePosition)
-            return;
+        if(numberOne == NULL)
+            length = strlen(row);
+        else length = numberOne - onePosition;
+        
+        length = length/(sizeof(char));
+        char* tempNumber = malloc(length + 1);
+        tempNumber[0] = '\n';
+        memcpy(tempNumber, onePosition, length);
+        sum = atoi(tempNumber);
+        free(tempNumber);
     }
         
-    else if(twoPosition[0] != '.')
+    if(twoPosition[0] != '.')
     {
         printf("found number");
 
         while(1)
         {
-            if( (--twoPosition)[0] == '.')
+            twoPosition = twoPosition - sizeof(char);            
+
+            if(( (twoPosition)[0] == '.') || (twoPosition < row) )
              break;
         }
         ++twoPosition;
         // find end of string
-        char* number = strtok(twoPosition, ".");
+        numberTwo = strchr(twoPosition, '.');
 
-        number = strtok(NULL, ".");
-        if(number > threePosition)
-            return;
+        int length = 0;
+
+        if(numberTwo == NULL)
+            length = strlen(row);
+        else length = numberTwo - twoPosition;
+
+        // was this part of the previous number?
+        if(twoPosition != onePosition)
+        {
+            length = numberTwo - twoPosition;
+            length = length/(sizeof(char));
+            char* tempNumber = malloc(length + 1);
+            tempNumber[0] = '\n';
+            memcpy(tempNumber, twoPosition, length);
+            sum += atoi(tempNumber);
+            free(tempNumber);
+        }
+        else ;
 
     }
 
@@ -68,14 +87,36 @@ void findNumbers(char* row, int offset)
         
         while(1)
         {
-            if( (--threePosition)[0] == '.')
+            threePosition = threePosition - sizeof(char);            
+
+            if(( (threePosition)[0] == '.') || (threePosition < row) )
              break;
         }
         ++threePosition;
         // find end of string
-        char* number = strtok(threePosition, ".");
-    }
+        numberThree = strchr(threePosition, '.');
 
+        int length = 0;
+
+        if(numberThree == NULL)
+            length = strlen(row);
+        else length = numberThree - threePosition;
+
+
+        // was this part of the previous number?
+        if((threePosition != onePosition) && (threePosition != twoPosition))
+        {
+            length = numberThree - threePosition;
+            length = length/(sizeof(char));
+            char* tempNumber = malloc(length + 1);
+            tempNumber[0] = '\n';
+            memcpy(tempNumber, threePosition, length);
+            sum += atoi(tempNumber);
+            free(tempNumber);
+        }
+        else ;
+    }
+    return sum;
 }
 
 int findSymbol(char* row, char symbol, char** symbolPtr)
@@ -96,14 +137,27 @@ int findSymbol(char* row, char symbol, char** symbolPtr)
 int main(int argc, void* argv)
 {
     char* map[] = { "..413.5...",
-                    ".1.23*.#9."};
+                    ".1.23*.#9.",
+                    "1234567890"};
 
     char symbol = '*';
     char* symbolPtr;
     symbolPtr = "deadbeef";
+    int totalSum = 0;
+    int rowSum = 0;
+
 printf("%p\n", &symbolPtr);
     int offset = findSymbol(map[1], symbol, &symbolPtr);
     if(symbolPtr != NULL)
-        findNumbers(map[0], offset);
+    {
+        rowSum = findNumbers(map[0], offset);
+        totalSum += rowSum;
+        rowSum = findNumbers(map[1], offset);
+        totalSum += rowSum;
+        rowSum = findNumbers(map[2], offset);
+        totalSum += rowSum;
+    }
+
+    printf("total sum: %d\n", totalSum);
                      
 }
